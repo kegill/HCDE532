7c3e0f5e7b76ea7cc1654fbaa45ef265a35e5218
Mike Sinkula <mike@mikesinkula.com>
2016-02-18 18:45:19 -0800

    A Contact Form for Your Website

--- 2016/contact.php
+++ 2016/contact.php
@@ -2,21 +2,47 @@
 
 	<!-- Begin Text -->
 	<article id="text">
-    	<h2>Contact</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Sed sit amet risus eget</strong> tortor bibendum aliquam eget in mi. Phasellus ultrices rutrum nisl in vestibulum. Ut sodales nisl in felis suscipit in sollicitudin metus varius. <em>Pellentesque porttitor</em> nibh quis nisi <a href="#">aliquam quis feugiat</a> odio fermentum. Nulla a enim eu est congue pellentesque sit amet a lacus. 
-        </p>
-        <h2>Header Level Two</h2>
-        <img class="left" src="images/FPO-photo-140x140.png" alt="Shut Up!">
-        <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer lectus turpis, venenatis quis sodales vitae, adipiscing ac dolor. Pellentesque rhoncus, ligula vitae blandit volutpat, arcu enim vehicula risus, at facilisis turpis turpis eget odio. Duis faucibus sodales pretium. Etiam suscipit magna metus. Aliquam vitae imperdiet est.</p>
-        <ul>
-        <li>bullet one</li>
-        <li>bullet two</li>
-        </ul>
-        <p>In a metus neque. Donec sed est metus, in cursus lectus. Aliquam vel aliquam ante. Curabitur non libero ut ipsum eleifend cursus. Sed rutrum sagittis pulvinar. Suspendisse potenti. Maecenas ut sapien nisi. Ut accumsan varius ornare. Quisque euismod sapien ut erat fermentum sed viverra nibh congue. Sed at metus est.</p>
-        <ol>
-        <li>number one</li>
-        <li>number two</li>
-        </ol>
+    	<h2>Contact Me!</h2>
+        
+        <?php
+
+		if ( isset ($_POST['submit']) ) { // if the submit button is clicked...
+		
+			$mailMesage = ("{$_POST['name']} {$_POST['email']} wrote: \n\n{$_POST['message']}"); // message of the email
+			
+			$mailSubject = "{$_POST['subject']}"; // subject of the email
+			
+			$mailRecipient = "sinkum@uw.edu"; // destination email address
+			
+			$mailSender = "From: {$_POST['email']}"; // the mail sender
+			
+			mail ($mailRecipient, $mailSubject , $mailMesage, $mailSender); // function to send the email
+			
+			print ("<p>Thank you for contacting me, <strong>{$_POST['name']}</strong>!</p>\n"); // notice that thanks the user
+		
+		}
+			
+		?>
+        
+        
+        <!-- Begin Contact Form -->
+        <form action="contact.php" method="post" name="contact" >
+        <label for="name">Name:</label>
+        <input name="name" type="text" placeholder="Put your name here." required="required" >
+        <label for="email">Email:</label>
+        <input name="email" type="email" placeholder="name@domain.com" required="required">
+        <label for="subject">Subject:</label>
+        <select name="subject">
+            <option value="Subject One">Subject 1</option>
+            <option value="Subject Two">Subject 2</option>
+            <option value="Subject Three" selected="selected">Subject 3</option>
+        </select>
+        <label for="message">Message:</label>
+        <textarea name="message" cols="40" rows="5" required="required"></textarea>
+        <input name="submit" type="submit" value="Submit Message" >
+        </form>
+        <!-- End Contact Form -->
+        
     </article>
     <!-- End Text -->
     
